ad2ac17f9e5ccb077fb80aca7f0597119b20fbe3
lbellmann <bellmann.louis@gmail.com>
2024-04-10 11:44:15 +0200

    Update Python setup - static method calls behaviour does not match Python 3.9, removed it - set python version >= 3.10 in setup.py - upgraded setup python action to v5

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ print(setuptools.find_packages())
 setuptools.setup(
     name='graphxplore',
     version='0.9.0',
-    description='meta data extraction, cleaning, and transformation as well as data exploration using association graphs and dashboards',
+    description='meta data extraction, cleaning, and transformation as well as data exploration using graph representations and dashboards',
     author='Louis Bellmann',
     url='https://github.com/UKEIAM/graphxplore',
     packages=setuptools.find_packages(),
-    requires=['py2neo', 'chardet']
+    python_requires='>=3.10',
+    requires=['py2neo', 'chardet', 'plotly']
 )
